aaa4cc00ae5072ec9eb2c770aaeaeda4da0decaf
Ben Aguirre <baguirre@luc.edu>
2024-05-16 12:37:51 -0400

    bug fices

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,7 +1,7 @@
-const asyncHandler = require('express-async-handler');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const User = require('../models/user');
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
 // Not really used, but created to practice using token and authdata
 exports.login_get = asyncHandler(async (req, res, next) => {
@@ -10,7 +10,7 @@ exports.login_get = asyncHandler(async (req, res, next) => {
       res.sendStatus(403);
     } else {
       res.json({
-        message: 'message',
+        message: "message",
         authData,
       });
     }
@@ -18,20 +18,32 @@ exports.login_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.login_post = asyncHandler(async (req, res, next) => {
-  const user = await User.findOne({ username: req.body.username.toLowerCase() });
+  const user = await User.findOne({
+    username: req.body.username.toLowerCase(),
+  });
+
+  let rsp = {
+    loggedin: false,
+    errors: "",
+  };
+
   if (!user) {
-    res.json('user not found');
+    rsp.errors = "User not found";
+    res.json(rsp);
   }
   const match = await bcrypt.compare(req.body.password, user.password);
   if (!match) {
-    res.json('password incorrect');
+    rsp.errors = "Incorrect password!";
+    res.json(rsp);
+  } else {
+    rsp.loggedin = true;
   }
-
   // Returns the token, as well as the user information,
   // save this into the LS and you are set to make safe requests after this
   jwt.sign({ user }, process.env.key, (err, token) => {
     res.json({
       token,
+      rsp,
     });
   });
 });
