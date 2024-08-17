10c110a663f033022e84e47d4abfb0111782540d
Ben Aguirre <baguirre@luc.edu>
2024-08-17 17:32:40 -0400

    fix origin

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(process.env.MongoDB);
 }
 
-const indexRouter = require('./routes/index');
-const loginRouter = require('./routes/login');
-const signupRouter = require('./routes/signup');
-const postRouter = require('./routes/posts');
+const indexRouter = require("./routes/index");
+const loginRouter = require("./routes/login");
+const signupRouter = require("./routes/signup");
+const postRouter = require("./routes/posts");
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: 'GET,PUT,POST',
-  optionsSuccessStatus: 204,
-}));
+app.use(
+  cors({
+    origin: "https://top-blog-ba.netlify.app/",
+    methods: "GET,PUT,POST",
+    optionsSuccessStatus: 204,
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/signup', signupRouter);
-app.use('/posts', postRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
+app.use("/login", loginRouter);
+app.use("/signup", signupRouter);
+app.use("/posts", postRouter);
 
 // FORMAT OF TOKEN
 // Authorization: Bearer <acces_token>
@@ -54,11 +56,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
