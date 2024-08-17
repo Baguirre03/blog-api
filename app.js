74edf17d6133ef50af1f27b266fdd6bf849538ac
Ben Aguirre <baguirre@luc.edu>
2024-08-17 17:34:28 -0400

    link fix

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://top-blog-ba.netlify.app/",
+    origin: "https://top-blog-ba.netlify.app",
     methods: "GET,PUT,POST",
     optionsSuccessStatus: 204,
   })
